1f4545f78c624ae5b2a6e28e20a3f6d2efc76d18
Johnny Bender <johnphillipbender@gmail.com>
2017-07-26 17:21:19 -0500

    changed files

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/launch/')
-def launch:
+def launch():
     return render_template('launch.html')
 
 @app.route('/index/')
-def index:
+def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
